0ad360c3c245b3c33cd6a05f9e2bebec95b2bd50
Jamie McNeil <jamievmcneil@gmail.com>
2024-12-17 12:26:59 -0500

    First iteration completed

---
+++ cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const minimist = require('minimist');
+const pkg = require('./index.js');
+
+const options = {
+  alias: { json: 'j' }
+};
+
+const argv = minimist(process.argv.slice(2), options);
+
+console.log(pkg(argv));
